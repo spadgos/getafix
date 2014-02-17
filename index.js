36a5b5adc8d5053dd1bbd80075f7ff08752a698d
Nick Fisher <spadgos@gmail.com>
2014-02-17 19:09:23 +0100

    Changes getafix to use promises instead of callbacks. Improves feedback while running

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var getafix = require('./src/getafix'),
-    _ = require('underscore');
-
-// export `build`
-if (module !== require.main) {
-  module.exports = getafix;
-} else { // or invoked directly
-  var args = require('yargs').argv,
-      dir = _.first(args._) || process.cwd();
-
-  getafix(dir, args, function () {
-    console.log('Done');
-  });
-}
+module.exports = require('./src/getafix');
--- src/getafix.js
+++ src/getafix.js
@@ -4,59 +4,84 @@ var _       = require('underscore'),
     coffee  = require('coffee-script'),
     debug   = require('debug')('getafix'),
     fs      = require('fs'),
-    glob    = require('glob'),
+    Glob    = require('glob'),
     Path    = require('path'),
     request = require('request'),
-    Url     = require('url');
+    Q       = require('q'),
+    Url     = require('url'),
 
-module.exports = function scraper(target, options, callback) {
+    readFile = Q.denodeify(fs.readFile),
+    glob     = Q.denodeify(Glob);
 
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
+
+module.exports = function (target, options) {
 
   options = _.extend({
     target: Path.resolve(target),
     onlyNew: false
   }, options);
 
-  async.parallel({
-    configs: readConfigs(target),
-    endpoints: function (next) {
-      glob(Path.join(target, '**/*.json'), function (err, files) {
-        async.filter(files.map(function (file) { return Path.resolve(file); }), filterItems(options), function (items) {
-          next(err, items);
-        });
+  return Q.all([
+    readConfigs(target),
+    glob(Path.join(target, '**/*.json')).then(function (files) {
+      var deferred = Q.defer();
+      async.filter(files.map(function (file) { return Path.resolve(file); }), filterItems(options), function (items) {
+        deferred.resolve(items);
       });
-    }
-  }, function (err, results) {
-    fetchItems(results.endpoints, results.configs, options, callback);
+      return deferred.promise;
+    })
+  ]).spread(function (configs, endpoints) {
+    return fetchItems(endpoints, configs, options);
   });
 };
 
-function fetchItems(files, configs, options, callback) {
-  async.each(files, function (file, next) {
-    var config = getConfig(file, configs, options);
-    if (!config) {
-      next();
-      return;
-    }
-    debug('Updating: ' + config.url);
-    request.get(
-      _.extend({ json: true }, _.pick(config, 'url', 'headers')),
-      function (err, response, body) {
-        var code = response && response.statusCode,
-            success = (!err && code < 300);
-        debug('Response: ' + code + ' for ' + config.url);
-        if (success) {
-          body = JSON.stringify(body, null, 2);
-          debug('Writing ' + Buffer.byteLength(body) + ' bytes to ' + file);
-          fs.writeFile(file, body, 'utf8', next);
+function fetchItems(files, configs, options) {
+  var defer = Q.defer(), promises, done = 0;
+  promises = files
+    .map(function (file) {
+      var config = getConfig(file, configs, options);
+      return config ? { file: file, config: config } : null;
+    })
+    .filter(_.identity)
+    .map(function (item) {
+      var config = item.config,
+          file = item.file,
+          deferred = Q.defer();
+
+      debug('Updating: ' + config.url);
+      request.get(
+        _.extend({ json: true }, _.pick(config, 'url', 'headers')),
+        function (err, response, body) {
+          var code = response && response.statusCode,
+              success = (!err && code < 300);
+          debug('Response: ' + code + ' for ' + config.url);
+          if (success) {
+            body = JSON.stringify(body, null, 2);
+            debug('Writing ' + Buffer.byteLength(body) + ' bytes to ' + file);
+            fs.writeFile(file, body, 'utf8', function (err) {
+              if (err) {
+                deferred.reject(err);
+              } else {
+                deferred.resolve();
+              }
+            });
+          } else {
+            deferred.reject(err);
+          }
         }
-      }
-    );
-  }, callback);
+      );
+      return deferred.promise.then(function () {
+        ++done;
+        defer.notify({ done: done, total: promises.length, file: file });
+      });
+    });
+
+  Q.all(promises).then(
+    defer.resolve.bind(defer),
+    defer.reject.bind(defer)
+  );
+
+  return defer.promise;
 }
 
 function getConfig(file, configs, options) {
@@ -120,22 +145,24 @@ function filterItems (options) {
 }
 
 function readConfigs(target) {
-  return function (callback) {
-    async.waterfall([
-      glob.bind(glob, Path.join(target, '**/.getafix')),
-      function (files, cb) {
-        async.map(files, function (file, next) {
-          fs.readFile(file, 'utf8', next);
-        }, function (err, contents) {
-          cb(err, contents.reduce(function (memo, content, i) {
-            /*jshint evil: true */
-            memo[Path.resolve(Path.dirname(files[i]))] = coffee.eval(content);
-            return memo;
-          }, {}));
-        });
-      }
-    ], callback);
-  };
+  var fileNames;
+
+  return glob(Path.join(target, '**/.getafix'))
+    .then(function (files) {
+      fileNames = files;
+      return Q.all(files.map(getContents));
+    })
+    .then(function (contents) {
+      return contents.reduce(function (memo, content, i) {
+        /*jshint evil: true */
+        memo[Path.resolve(Path.dirname(fileNames[i]))] = coffee.eval(content);
+        return memo;
+      }, {});
+    });
+
+  function getContents(file) {
+    return readFile(file, 'utf8');
+  }
 }
 
 function mergeConfig(config, thisConf) {
