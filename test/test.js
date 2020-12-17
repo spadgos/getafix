11785d0d867e6754ce1f5095776eb11c537e5ee6
Dave Brotherstone <dave.brotherstone@soundcloud.com>
2020-12-18 00:02:43 +0100

    Modernise to async/await & update deps
    This is effectively a rewrite to use async/await, real promises (to
    remove the dependency on `q`), `got` instead of `request`, `jest`
    instead of mocha with expect.js.
    
    This removes a bunch of security vulnerabilities from outdated
    dependencies, but also makes the code a bit simpler.
    
    This does mean node prior to v7.6 isn't supported, but that doesn't seem
    like much of an issue.

--- test/test.js
+++ test/test.js
@@ -1,149 +1,154 @@
-/*globals it, describe, after, afterEach */
-
-var getafix = require('../index.js'),
-    _       = require('underscore'),
-    debug   = require('debug')('test'),
-    expect  = require('expect.js'),
-    fs      = require('fs'),
-    mkdirp  = require('mkdirp'),
-    Path    = require('path'),
-    Q       = require('q'),
-    rimraf  = require('rimraf'),
-    sinon   = require('sinon'),
-    touch   = require('touch'),
-    TMP     = __dirname + '/tmp';
+const getafix = require('../index.js');
+const _ = require('underscore');
+const debug = require('debug')('test');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const Path = require('path');
+const rimraf = require('rimraf');
+const sinon = require('sinon');
+const touch = require('touch');
+const TMP = __dirname + '/tmp';
 
 describe('Configuration', function () {
-  afterEach(function () {
-    _.result(getafix.request, 'restore');
+  let notify;
+
+  beforeEach(() => {
+    notify = sinon.spy();
   });
 
-  after(function () {
+  afterEach(function () {
+    _.result(getafix.got, 'restore');
+  });
+
+  afterAll(function () {
     rimraf.sync(TMP);
   });
 
   describe('read from getafix files', function () {
-    it('fetches data and stores it in json files', function (done) {
+    it('fetches data and stores it in json files', async function () {
       makeStructure({
         '.getafix': 'base: "http://example.com"',
         users: {
           '2.json': true,
           2: {
-            'tracks.json': true
-          }
-        }
+            'tracks.json': true,
+          },
+        },
       });
 
       stubAjax({
-        'http://example.com/users/2': { id: 2, username: 'eric', full_name: 'Eric Wahlforss' },
+        'http://example.com/users/2': {
+          id: 2,
+          username: 'eric',
+          full_name: 'Eric Wahlforss',
+        },
         'http://example.com/users/2/tracks': [
           { id: 10, title: 'flickermood' },
-          { id: 11, title: 'Nox' }
-        ]
+          { id: 11, title: 'Nox' },
+        ],
       });
-      getafix(TMP).then(function () {
-        var user = require(Path.join(TMP, 'users', '2.json')),
-            tracks = require(Path.join(TMP, 'users', '2', 'tracks.json'));
-        expect(user).to.have.property('username', 'eric');
-        expect(tracks).to.be.an('array');
-        expect(tracks[0]).to.have.property('title', 'flickermood');
-        expect(tracks[1]).to.have.property('title', 'Nox');
-        done();
-      }).catch(done);
+      await getafix(TMP, notify);
+      const user = require(Path.join(TMP, 'users', '2.json'));
+      const tracks = require(Path.join(TMP, 'users', '2', 'tracks.json'));
+      expect(user).toMatchObject({ username: 'eric' });
+      expect(tracks).toMatchObject([
+        { title: 'flickermood' },
+        { title: 'Nox' },
+      ]);
     });
 
-    it('supports formats other than json', function (done) {
+    it('supports formats other than json', async function () {
       makeStructure({
         '.getafix': 'base: "http://example.com"',
         users: {
           '2.xml': true,
           2: {
-            'tracks.txt': true
-          }
-        }
+            'tracks.txt': true,
+          },
+        },
       });
 
-      var userRawResp = '<?xml version="1.0" encoding="UTF-8" ?><response></response>',
-          tracksRawResp = 'Got some tracks as a text file!';
+      const userRawResp =
+        '<?xml version="1.0" encoding="UTF-8" ?><response></response>';
+      const tracksRawResp = 'Got some tracks as a text file!';
 
       stubAjax({
         'http://example.com/users/2': userRawResp,
-        'http://example.com/users/2/tracks': tracksRawResp
+        'http://example.com/users/2/tracks': tracksRawResp,
       });
-      getafix(TMP).then(function () {
-        var user = fs.readFileSync(Path.join(TMP, 'users', '2.xml'), 'utf8'),
-            tracks = fs.readFileSync(Path.join(TMP, 'users', '2', 'tracks.txt'), 'utf8');
-        expect(user).to.equal(userRawResp + '\n');
-        expect(tracks).to.equal(tracksRawResp + '\n');
-        done();
-      }).catch(done);
+      await getafix(TMP, notify);
+      const user = fs.readFileSync(Path.join(TMP, 'users', '2.xml'), 'utf8');
+      const tracks = fs.readFileSync(
+        Path.join(TMP, 'users', '2', 'tracks.txt'),
+        'utf8'
+      );
+      expect(user).toEqual(userRawResp);
+      expect(tracks).toEqual(tracksRawResp);
     });
 
-    it('merges multiple configuration files', function (done) {
+    it('merges multiple configuration files', async function () {
       makeStructure({
         '.getafix': [
           'base: "http://example.com"',
           'query:',
           '  foo: 1',
-          '  bar: 2'
+          '  bar: 2',
         ].join('\n'),
         users: {
-          '2.json': true
+          '2.json': true,
         },
         tracks: {
-          '.getafix': [
-            'query:',
-            '  foo: null',
-            '  quux: 3'
-          ].join('\n'),
-          '10.json': true
-        }
+          '.getafix': ['query:', '  foo: null', '  quux: 3'].join('\n'),
+          '10.json': true,
+        },
       });
 
       stubAjax({
         'http://example.com/users/2?foo=1&bar=2': { id: 2, username: 'eric' },
-        'http://example.com/tracks/10?bar=2&quux=3': { id: 10, title: 'flickermood' }
+        'http://example.com/tracks/10?bar=2&quux=3': {
+          id: 10,
+          title: 'flickermood',
+        },
       });
 
-      getafix(TMP).then(function () {
-        var user = require(Path.join(TMP, 'users', '2.json')),
-            track = require(Path.join(TMP, 'tracks', '10.json'));
-        expect(user).to.have.property('username', 'eric');
-        expect(track).to.have.property('title', 'flickermood');
-        done();
-      }).catch(done);
+      await getafix(TMP, notify);
+      const user = require(Path.join(TMP, 'users', '2.json'));
+      const track = require(Path.join(TMP, 'tracks', '10.json'));
+      expect(user).toMatchObject({ username: 'eric' });
+      expect(track).toMatchObject({ title: 'flickermood' });
     });
 
-    it('which must be valid Coffeescript', function (done) {
+    it('which must be valid Coffeescript', async function () {
       makeStructure({
-        '.getafix': '3adadt14361 113 some invalid content!!'
+        '.getafix': '3adadt14361 113 some invalid content!!',
       });
-      getafix(TMP).then(function () {
-        done(new Error('getafix should have failed'));
-      }, function (err) {
-        expect(err.message).to.match(/Error in .*\/\.getafix/);
-        done();
-      });
+      return expect(getafix(TMP, notify)).rejects.toThrow(/Error in .*\/\.getafix/);
     });
 
-    it('supports functions in configuration files', function (done) {
+    it('supports functions in configuration files', async function () {
       makeStructure({
-        '.getafix': [
-          'map: (path) -> \'/resolve?url=example.com/\' + path'
-        ].join('\n')
+        '.getafix': ["map: (path) -> '/resolve?url=example.com/' + path"].join(
+          '\n'
+        ),
       });
-      getafix(TMP).then(function () {
-        done();
-      }, done);
+      await getafix(TMP, notify);
     });
   });
 });
 
 function stubAjax(responses) {
-  sinon.stub(getafix, 'request', function (options) {
+  sinon.stub(getafix, 'got').callsFake(function (options) {
     debug('Intercepted request for ', options.url);
-    expect(responses).to.have.property(options.url);
-    return Q.resolve([{ statusCode: 200 }, responses[options.url]]);
+    expect(responses).toMatchObject({ [options.url]: expect.anything() });
+    return Promise.resolve({
+      statusCode: 200,
+      rawBody: Buffer.from(
+        typeof responses[options.url] === 'string'
+          ? responses[options.url]
+          : JSON.stringify(responses[options.url]),
+        'utf8'
+      ),
+    });
   });
 }
 
